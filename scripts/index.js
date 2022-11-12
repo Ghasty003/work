eac4df2262e4d2d5b263d8f9cf142d1962d2420b
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-11-12 14:50:55 -0800

    Initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -22,3 +22,16 @@ function includeHTML() {
   }
 }
 includeHTML();
+
+// Script for signup.html
+
+const showPassword = document.querySelector(".see-pass");
+const input = document.querySelector(".password input");
+
+showPassword.addEventListener("click", () => {
+  if (input.type === "text") {
+    input.type = "password";
+  } else if (input.type === "password") {
+    input.type = "text";
+  }
+});
